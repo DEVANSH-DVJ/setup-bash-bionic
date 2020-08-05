7b41b3cc26e24c25bce568d07db45569ff99526a
devansh-dvj <devansh.dvj@gmail.com>
2020-08-05 16:29:10 +0530

    Added VPN Setup

---
+++ VPN Setup.sh
@@ -0,0 +1,4 @@
+## For IITB VPN, go to https://www.cc.iitb.ac.in/ and follow the steps
+sudo apt update;
+sudo apt -y upgarde;
+sudo apt install network-manager-openvpn network-manager-openvpn-gnome;
