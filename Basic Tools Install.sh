9622e133c890dcb41b44307332f48555726ab790
devansh-dvj <devansh.dvj@gmail.com>
2020-08-05 16:30:17 +0530

    Added other Basic Tools - vim, g++, nodejs, yarn

--- Basic Tools Install.sh
+++ Basic Tools Install.sh
@@ -1,6 +1,25 @@
-sudo apt install -y git;
-sudo apt install -y tree;
-sudo apt install -y terminator;
+sudo apt -y install git;
+sudo apt -y install tree;
+sudo apt -y install terminator;
+sudo apt -y install bash-completion;
+
+sudo apt -y remove vim-tiny;
+sudo apt -y install vim;
+
+sudo apt -y install curl;
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
+sudo apt -y install nodejs;
+
+sudo apt -y install gcc g++ make;
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
+sudo apt update;
+sudo apt -y install yarn;
+
+echo "
+alias sb='source ~/.bashrc';
+" >> ~/.bashrc;
 
 ## Reboot once to be safe
 # sudo reboot;
