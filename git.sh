1356ec43b4821b8de24348c5dd7aa9788f27cc6e
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 02:50:52 +0530

    Added git install and configure script

---
+++ git.sh
@@ -0,0 +1,26 @@
+sudo apt -y install git;
+
+## Git configurations
+git config --global user.name "devansh-dvj";
+git config --global user.email "devansh.dvj@gmail.com";
+echo "[user]
+	name = devansh-dvj
+	email = devansh.dvj@gmail.com
+[alias]
+  co = checkout
+  ci = commit
+  st = status
+  br = branch
+  hist = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+" > ~/.gitconfig;
+
+## Test
+# cat ~/.gitconfig;
+
+## For ssh setup
+ssh-keygen -t rsa -b 4096 -C "devansh.dvj@gmail.com";
+ssh-add ~/.ssh/id_rsa;
+sudo apt -y install xclip;
+xclip -sel clip < ~/.ssh/id_rsa.pub;
+echo "The ssh rsa key has been copied to the clipboard, paste it while creating a new ssh key at https://github.com/settings/keys."
+sleep 10;
