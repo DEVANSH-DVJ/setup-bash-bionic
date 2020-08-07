3d128215addf8da2243165cf0d58bf8f4a567123
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 03:28:03 +0530

    Added ROS installation script

--- big-installs/ros.sh
+++ big-installs/ros.sh
@@ -21,7 +21,7 @@ git checkout melodic-devel;
 echo "source ~/catkin_ws/devel/setup.bash;
 source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_bash;
 source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_aliases;
-" >> ~/.bashrc;
+alias wrapper.py='python3 ~/catkin_ws/src/robosub/auv_utils/src/utility/wrapper/wrapper.py'" >> ~/.bashrc;
 source ~/.bashrc;
 
 sudo apt -y install libfftw3-dev;
