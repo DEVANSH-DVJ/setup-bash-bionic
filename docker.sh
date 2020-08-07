d1e71db727d662d2856e34b6126dbd6520143df2
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 02:57:16 +0530

    Added docker installation

---
+++ docker.sh
@@ -0,0 +1,15 @@
+sudo apt -y remove docker docker-engine docker.io containerd runc;
+sudo apt update;
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+## Test
+# sudo apt-key fingerprint 0EBFCD88;
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+
+sudo apt update;
+sudo apt -y install docker-ce docker-ce-cli containerd.io;
+sudo docker run hello-world;
+sudo groupadd docker;
+sudo usermod -aG docker $USER;
+newgrp docker;
+docker run hello-world;
