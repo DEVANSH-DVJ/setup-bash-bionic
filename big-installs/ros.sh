60532f2c5466dbea1ef7852ca8bbb26bbad1a7eb
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 02:56:16 +0530

    Added ROS installation script

---
+++ big-installs/ros.sh
@@ -0,0 +1,35 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654;
+sudo apt update;
+sudo apt -y install ros-melodic-desktop-full;
+sudo rosdep init;
+sudo rosdep update;
+sudo rosdep fix-permissions;
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc;
+source ~/.bashrc;
+sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential;
+
+mkdir -p ~/catkin_ws/src;
+cd ~/catkin_ws;
+catkin_make;
+source devel/setup.bash;
+
+cd src;
+git clone git@github.com:auv-iitb/robosub.git;
+cd robosub;
+git checkout melodic-devel;
+echo "source ~/catkin_ws/devel/setup.bash;
+source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_bash;
+source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_aliases;
+" >> ~/.bashrc;
+source ~/.bashrc;
+
+sudo apt -y install libfftw3-dev;
+sudo apt -y install ros-melodic-bfl;
+sudo apt -y install libpcap-dev;
+sudo apt -y install libpugixml-dev;
+sudo apt -y install qt4-default;
+sudo apt -y install ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control;
+
+cd ~/catkin_ws;
+catkin_make;
