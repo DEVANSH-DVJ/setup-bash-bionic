68f439fbbfbd0d927820d252647ee4abe85a725e
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 02:51:55 +0530

    Added Basic tools script

---
+++ Basic Tools.sh
@@ -0,0 +1,30 @@
+sudo apt -y install terminator;
+sudo apt -y install tree;
+sudo apt -y install bash-completion;
+sudo apt -y install python-argcomplete python3-argcomplete;
+sudo apt -y install flake8 isort;
+sudo apt -y install pylint clang-format;
+
+bash ./vim/Install.sh;
+bash ./vim/Configurations.sh;
+
+bash ./browsers/Chrome.sh;
+bash ./browsers/Opera.sh;
+
+sudo apt -y install curl;
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;
+sudo apt -y install nodejs;
+
+sudo apt -y install gcc g++ make;
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
+sudo apt update;
+sudo apt -y install yarn;
+
+echo "
+alias sb='source ~/.bashrc';
+" >> ~/.bashrc;
+
+## Reboot once to be safe
+# sudo reboot;
