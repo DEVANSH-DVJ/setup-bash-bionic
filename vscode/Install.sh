d26487162ebb0d7775751192371f41592e406c51
devansh-dvj <devansh.dvj@gmail.com>
2020-08-08 03:28:22 +0530

    Added VSCode Settings

--- vscode/Install.sh
+++ vscode/Install.sh
@@ -4,3 +4,13 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main";
 sudo apt update;
 sudo apt -y install code;
+
+## Extensions:
+# Bash Beautify
+# Docker
+# Git History
+# Markdown Shortcuts
+# Python
+# Remote Development
+# shell-format
+# Trailing Spaces
