3d9ed357541bd4878712f181062ecadbe036453f
devansh-dvj <devansh.dvj@gmail.com>
2021-01-10 02:14:46 +0530

    Java added

---
+++ java.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:linuxuprising/java;
+sudo apt update;
+sudo apt upgrade -y;
+sudo apt install oracle-java15-installer;
--- windows/windows_2.sh
+++ windows/windows_2.sh
@@ -4,4 +4,3 @@ ln -s ~/Windows-D/EVERYTHING/ ~/Desktop/EVERYTHING;
 ln -s ~/Windows-D/EVERYTHING/IITB/ ~/Desktop/IITB;
 ln -s ~/Windows-D/EVERYTHING/BOOKS/ ~/Desktop/BOOKS;
 ln -s ~/Windows-D/EVERYTHING/PROJECTS/ ~/Desktop/PROJECTS;
-ln -s ~/Windows-D/EVERYTHING/PROJECTS/WORK/ ~/PROJECTS;
