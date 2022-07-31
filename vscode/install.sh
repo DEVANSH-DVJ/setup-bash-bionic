3685486959033bbbae3ebf957703ec67283f9e9e
devansh-dvj <devansh.dvj@gmail.com>
2022-08-01 03:00:12 +0530

    Updated vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,36 +1,10 @@
 sudo apt update;
-sudo apt -y install software-properties-common apt-transport-https wget;
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main";
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/vscode.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update;
 sudo apt -y install code;
 
-## Extensions:
-# AWK
-# Bash Beautify
-# Beautify
-# C/C++
-# C++ Intellisense
-# Clang-Format
-# Code-Runner
-# Code Spell Checker
-# ES Lint
-# Excel Viewer
-# Git History
-# Git Lens
-# Jupyter
-# LaTeX Workshop
-# Markdown Shortcuts
-# Matlab
-# Modern VHDL
-# Prettier - Code formatter
-# Pylance
-# Python
-# ROS
-# shell-format
-# Trailing Spaces
-# VHDL
-# VHDL Formatter
-# VHDL Language Server
-# VSC-Prolog
-# vscode-icons
+## Test VSCode once
+# code .;
+
+## Sync the settings
