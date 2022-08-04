f3f152863aca70006a68812e35e2f55c6df5a7b9
devansh-dvj <devansh.dvj@gmail.com>
2022-08-04 08:21:41 +0530

    Updated chrome

--- browsers/chrome.sh
+++ browsers/chrome.sh
@@ -1,9 +1,10 @@
 sudo apt update;
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrom-keyring.gpg;
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrom-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list;
+sudo apt update;
+sudo apt -y install google-chrome-stable;
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb;
-sudo dpkg -i google-chrome-stable_current_amd64.deb;
-sudo apt install -fy;
-sudo rm -rf google-chrome-stable_current_amd64.deb;
 ## Test Chrome once
 # google-chrome;
+
 ## Sync the account and customize the settings
