24658583abe7de9e34e948bbdb54a27d5142b52d
devansh-dvj <devansh.dvj@gmail.com>
2022-08-04 08:15:32 +0530

    Updated opera

--- browsers/opera.sh
+++ browsers/opera.sh
@@ -1,7 +1,11 @@
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free";
+sudo apt update;
+curl -fsSL https://deb.opera.com/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/opera.gpg;
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/opera.gpg] https://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera.list
 sudo apt update;
 sudo apt -y install opera-stable;
+sudo rm -rf /etc/apt/sources.list.d/opera-stable.list;
+
 ## Test Opera once
 # opera;
+
 ## Get the bookmarks and customize the settings
